d3dab18ccdb0831bbcb8862a8c76cd462ba628e3
Your Name <you@example.com>
2018-12-18 14:48:37 -0700

    commit

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Anton Miguel de Guia on 2018-12-18.
+//  Copyright © 2018 Anton De Guia. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+}
+
