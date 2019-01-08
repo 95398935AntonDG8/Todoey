3948701cad441d2fc6e3448b2416119826c14b9d
Your Name <you@example.com>
2019-01-08 14:08:25 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Anton Miguel de Guia on 2018-12-20.
+//  Copyright © 2018 Anton De Guia. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
